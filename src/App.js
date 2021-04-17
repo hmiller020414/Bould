675e2afc2e874ac7b083796246753be4526dc6c6
Hayley Miller <hayleymm14@gmail.com>
2021-04-17 12:50:16 -0500

    add functionality to close out of the current climb you are viewing, upload checkmark and close icons

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import Walls from './services/Walls';
 import Wall from './pages/Wall/Wall';
 import Climb from './pages/Climb/Climb';
 
+import WomanClimbing from './images/woman_climbing.jpg';
+
 
 const App = () => {
 
   return (
     <>
       <header>
-        <Link to='/'><img className="logo" src={logo} alt="Bould Logo" /></Link>
+        <Link to='/' className='logoLink'><img className="logo" src={logo} alt="Bould Logo" /></Link>
         <nav>
-          <Link to='/my-climbs'>my climbs</Link>
-          <a href='https://www.brooklynboulders.com' target="_blank" rel="noreferrer">join the gym</a>
+          <Link to='/my-climbs' className='navLink'>my climbs</Link>
+          <a href='https://www.brooklynboulders.com' className='navLink' target="_blank" rel="noreferrer">join the gym</a>
         </nav>
       </header>
       <main>
@@ -30,15 +32,16 @@ const App = () => {
           <Route exact path="/">
             <div className="homepageContent">
               <div className="homepageImage">
-              <img src='images/woman_climbing.jpg' alt='woman bouldering' />
+              <img src={WomanClimbing} alt='woman bouldering' />
               </div>
               <div className="wallListContainer">
+                <p>select a wall to see our current routes</p>
                 <ul className="wallList">
-                  <Link to='/wall/arch'><li>arch wall</li></Link>
-                  <Link to='/wall/back'><li>back wall</li></Link>
-                  <Link to='/wall/hole'><li>hole in the wall</li></Link>
-                  <Link to='/wall/inside'><li>inside wall</li></Link>
-                  <Link to='/wall/slant'><li>slant wall</li></Link>
+                  <Link to='/wall/arch' className='arch'><li>arch wall</li></Link>
+                  <Link to='/wall/back' className='back'><li>back wall</li></Link>
+                  <Link to='/wall/hole' className='hole'><li>hole in the wall</li></Link>
+                  <Link to='/wall/inside' className='inside'><li>inside wall</li></Link>
+                  <Link to='/wall/slant' className='slant'><li>slant wall</li></Link>
                 </ul>
               </div>
             </div>
--- src/pages/Climb/Climb.js
+++ src/pages/Climb/Climb.js
@@ -1,3 +1,8 @@
+import { Link } from 'react-router-dom';
+
+import CloseIcon from '../../images/close-x-icon.png'
+import Checkmark from '../../images/checkmark.png'
+
 const Climb = ({match, Walls}) => {
 
     const id = parseInt(match.params.id)
@@ -14,15 +19,17 @@ const Climb = ({match, Walls}) => {
                 <h1>Bould</h1>
                 <p>moves</p>
                 <p>click the check to save the details of your climb</p>
+                <img src={Checkmark} alt='checkmark' />
             </div>
             <div className='singleClimb'>
                 <div className='singleClimbInner'>
                     <div className='singleClimbImg'>
                         <img src={climb.image} alt={`${climb.color} ${climb.rating} boulder route`} />
+                        <Link to={`/wall/${climb.location}`}><img src={CloseIcon} alt='close icon'/></Link>
                     </div>
                     <div className='singleClimbDetails'>
                         <p>this route:</p>
-                        <p>location: {climb.location} | rating: {climb.rating} | color: {climb.color}</p>
+                        <p>location: {climb.location} wall | rating: {climb.rating} | color: {climb.color}</p>
                     </div>
                 </div>
             </div>
