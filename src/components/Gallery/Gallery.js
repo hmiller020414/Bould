9244ded0eaa3e7d7c5c4168de58371393f97d7a6
Hayley Miller <hayleymm14@gmail.com>
2021-04-18 11:17:44 -0500

    css for wall page and gallery component

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 const Gallery = ({color, rating, image, id, location}) => {
 
     return (
-        <Link to={`/wall/${location}/${id}`}>
-            <img className='gallerySingle' src={image} alt={`${color} ${rating} boulder route`} />
+        <Link to={`/wall/${location}/${id}`} className='gallerySingle'>
+            <img className='galleryImg' src={image} alt={`${color} ${rating} boulder route`} />
         </Link>
     )
 
--- src/pages/Wall/Wall.js
+++ src/pages/Wall/Wall.js
@@ -8,16 +8,18 @@ const Wall = ({match, Walls}) => {
     const wallGallery = thisWall.length ? thisWall.map( c => <Gallery key={c.id} {...c} /> ): null;
     
     return (
-        <>
-            <div>
-                <h2>{`${slug} wall`}</h2>
+        <div className='wallContainer'>
+            <div className='wallContent'>
+                <h1 className={slug}>{`${slug} wall`}</h1>
+                <p>click on a climb to preview it</p>
+                <p>once you’ve climbed it, rate it, add your notes<br />and save it to your completed climbs</p>
             </div>
             <div className='galleryOuter'>
                 <div className='galleryInner'>
                     {wallGallery}
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
